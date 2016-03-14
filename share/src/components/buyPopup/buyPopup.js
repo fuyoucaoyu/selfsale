f0077ecd2b6a70ea4cf8d7396e02ddfbf8266af6
alvin zhang <fuyoucaoyu@163.com>
2016-03-14 12:56:00 +0800

    support logo

--- share/src/components/buyPopup/buyPopup.js
+++ share/src/components/buyPopup/buyPopup.js
@@ -2,12 +2,17 @@
  * @file
  */
 
+ var WorkdDisplay = require('../workDisplay/workDisplay.js');
+
 module.exports = {
     template: __inline('./buyPopup.html'),
-    props: ['options'],
+    props: ['options', 'displayImgs'],
     data: function () {
     },
     methods: {
+    },
+    components: {
+    	'work-display': WorkdDisplay
     },
     ready: function (argument) {
     }
