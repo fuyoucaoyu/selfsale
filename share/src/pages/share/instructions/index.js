ca5b180744a88becc70c6b4fc5bc2c0c42484f13
liuyi <liuyi@youku.com>
2016-03-15 16:11:30 +0800

    update material size html

--- share/src/pages/share/instructions/index.js
+++ share/src/pages/share/instructions/index.js
@@ -7,19 +7,36 @@ Vue.config.debug = true;
 Vue.use(window.tap);
 
 var generateUrl = function (pos, index) {
-	return 'url(../../../static/images/materialsize/' + pos + '/tab_btn_' + index + '.png)';
+	return 'url(../../../static/images/materialsize/' + pos + '/tab_btn_' + index + '.jpg)';
 }
-
+var generateTabUrl = function (pos, index) {
+    return 'url(../../../static/images/materialsize/' + pos + '/tab_ms_' + index + '.png)';
+}
 var app = new Vue({
     el: '#app',
     data: {
     	isSize: true,
         selectIndex: 1,
+        selectTabIndex: 1,
         leftUrl: generateUrl(1, 1),
         midUrl: generateUrl(2, 1),
-        rightUrl: generateUrl(3, 1)
+        rightUrl: generateUrl(3, 1),
+        leftTabUrl: generateTabUrl(1, 1),
+        rightTabUrl: generateTabUrl(2, 1)
     },
     methods: {
+        updateTbabUrl: function () {
+            this.$data.leftTabUrl = generateTabUrl(1, this.$data.selectTabIndex);
+            this.$data.rightTabUrl = generateTabUrl(2, this.$data.selectTabIndex);
+        },
+        clickLeftTabItem: function (event) {
+            this.$data.selectTabIndex = 1;
+            this.updateTbabUrl();
+        },
+        clickRightTabItem: function (event) {
+            this.$data.selectTabIndex = 2;
+            this.updateTbabUrl();
+        },
     	updateUrl: function () {
     		this.$data.leftUrl = generateUrl(1, this.$data.selectIndex);
     		this.$data.midUrl = generateUrl(2, this.$data.selectIndex);
