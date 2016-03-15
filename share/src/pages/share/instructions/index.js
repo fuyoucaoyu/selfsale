0955bcbc7ff04e8a00d8f01f563d39d2a9ce697e
alvin zhang <fuyoucaoyu@163.com>
2016-03-15 21:32:39 +0800

    refine instruction page

--- share/src/pages/share/instructions/index.js
+++ share/src/pages/share/instructions/index.js
@@ -12,32 +12,24 @@ var generateUrl = function (pos, index) {
 var generateTabUrl = function (pos, index) {
     return 'url(../../../static/images/materialsize/' + pos + '/tab_ms_' + index + '.png)';
 }
+
 var app = new Vue({
     el: '#app',
     data: {
     	isSize: true,
         selectIndex: 1,
-        selectTabIndex: 1,
-        showUrl: 'url(../../../static/images/materialsize/size_male.png)',
         leftUrl: generateUrl(1, 1),
         midUrl: generateUrl(2, 1),
         rightUrl: generateUrl(3, 1),
+        isMaleInstr: true,
+        isFemaleInstr: false,
+        isChildInstr: false,
+        instructionUrl: 'url(../../../static/images/materialsize/size_male.png)',
+        selectTabIndex: 1,
         leftTabUrl: generateTabUrl(1, 1),
         rightTabUrl: generateTabUrl(2, 1)
     },
     methods: {
-        updateTbabUrl: function () {
-            this.$data.leftTabUrl = generateTabUrl(1, this.$data.selectTabIndex);
-            this.$data.rightTabUrl = generateTabUrl(2, this.$data.selectTabIndex);
-        },
-        clickLeftTabItem: function (event) {
-            this.$data.selectTabIndex = 1;
-            this.updateTbabUrl();
-        },
-        clickRightTabItem: function (event) {
-            this.$data.selectTabIndex = 2;
-            this.updateTbabUrl();
-        },
     	updateUrl: function () {
     		this.$data.leftUrl = generateUrl(1, this.$data.selectIndex);
     		this.$data.midUrl = generateUrl(2, this.$data.selectIndex);
@@ -46,18 +38,48 @@ var app = new Vue({
     	clickLeftItem: function (event) {
     		this.$data.selectIndex = 1;
     		this.updateUrl();
-            this.$data.showUrl = 'url(../../../static/images/materialsize/size_male.png)';
+            this.showInstr();
     	},
     	clickMidItem: function (event) {
     		this.$data.selectIndex = 2;
     		this.updateUrl();
-            this.$data.showUrl ='url(../../../static/images/materialsize/size_female.png)';
+            this.showInstr();
     	},
     	clickRightItem: function (event) {
     		this.$data.selectIndex = 3;
     		this.updateUrl();
-            this.$data.showUrl = 'url(../../../static/images/materialsize/size_child.png)';
-    	}
+            this.showInstr();
+    	},
+        showInstr: function () {
+            var selectedIndex = this.$data.selectIndex;
+            this.$data.isMaleInstr = false;
+            this.$data.isFemaleInstr = false;
+            this.$data.isChildInstr = false;
+            if (3 === selectedIndex) {
+                this.$data.isChildInstr = true;
+                this.$data.instructionUrl = 'url(../../../static/images/materialsize/size_child.png)';
+            } else if (2 === selectedIndex) {
+                this.$data.isFemaleInstr = true;
+                this.$data.instructionUrl ='url(../../../static/images/materialsize/size_female.png)';
+            } else {
+                this.$data.isMaleInstr = true;
+                this.$data.instructionUrl = 'url(../../../static/images/materialsize/size_male.png)';
+            }
+        },
+        updateTabUrl: function () {
+            this.$data.leftTabUrl = generateTabUrl(1, this.$data.selectTabIndex);
+            this.$data.rightTabUrl = generateTabUrl(2, this.$data.selectTabIndex);
+        },
+        clickLeftTabItem: function (event) {
+            this.$data.isSize = true;
+            this.$data.selectTabIndex = 1;
+            this.updateTabUrl();
+        },
+        clickRightTabItem: function (event) {
+            this.$data.isSize = false;
+            this.$data.selectTabIndex = 2;
+            this.updateTabUrl();
+        }
     },
     components: {
     }
