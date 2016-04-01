d369a27193945da64b96fbfbc2f08ba629aa8709
alvin zhang <fuyoucaoyu@163.com>
2016-04-01 17:17:53 +0800

    add debug code

--- share/src/static/lib/util.js
+++ share/src/static/lib/util.js
@@ -481,6 +481,7 @@ var util = {
                     wx.config(config);
                     // 添加验证通过后的回调
                     wx.ready(function () {
+                        alert('微信安全认证成功');
                         // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
                         // 获取“分享到朋友圈”按钮点击状态及自定义分享内容接口
                         wx.onMenuShareTimeline({
@@ -509,6 +510,11 @@ var util = {
                                 // 用户取消分享后执行的回调函数
                             }
                         });
+                    });
+
+                    wx.error(function(res){
+                        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+                        alert('微信安全认证出错：' + res);
                     });
                 }
             });
